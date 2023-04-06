2e5c7e74fa2553420e12249880899c7a773c481b
SchinoccaO <124750475+SchinoccaO@users.noreply.github.com>
2023-04-05 21:39:08 -0300

    Add files via upload

--- problema3.js
+++ problema3.js
@@ -1,52 +1,81 @@
-// Crear una calculadora que simplifique el trabajo de todo: suma, resta, multip, division. 
+// problema de los ceulares. 
 
-// paso 1: funciones 
-
-
-const sumar = (num1, num2)=>{
-    return parseInt(num1) + parseInt(num2);
+class Celular{
+    constructor(color, peso, tamano, rdc, ram){
+        this.color = color;
+        this.peso = peso;
+        this.tamano = this.tamano;
+        this.resolucionDeCamara = rdc;
+        this.memoriaRam = ram;
+        this.encendido = false
+    }
+    presionarBotonEncedido(){
+        if (this.encendido == false){
+            alert('celular prendido');
+            this.encendido = true;
+        }else {
+            alert('celular apagado. ');
+            this.encendido = false;
+        }
+    }
+    reiniciar(){
+        if (this.encendido == true) {
+            alert('reiniciando el celular')
+        }else{
+            alert('el celular esta apagado.')
+        }
+    }
+    tomarFoto(){
+        alert(`se ha tomado la foto en una resolución de: ${this.resolucionDeCamara}`)
+    }
+    grabarVideo(){
+        alert(`se ha tomado el video en una resolución de ${this.resolucionDeCamara}`)
+    }
+    mobileInfo(){
+        return `
+        Color: <b>${this.color}</b></br> 
+        Peso: <b>${this.peso}</b></br>
+        Tamaño: <b>${this.tamano}</b></br>
+        Resolución de video: <b>${this.resolucionDeCamara}</b></br>
+        Memoria ram: <b>${this.memoriaRam}</b></br>`
+    }
 }
 
-const restar = (num1, num2)=>{
-    return parseInt(num1) - parseInt(num2);
-}
-const dividir = (num1, num2)=>{
-    return parseInt(num1) / parseInt(num2);
-}
-const multip = (num1, num2)=>{
-    return parseInt(num1) * parseInt(num2);
-}
-
-alert('que operacion desea realizar? ')
-let operacion = prompt('1. suma, 2.resta, 3. dividir, 4. multiplicar')
-
-if (operacion == 1){
-    let numero1 = prompt('primer numero apra sumar: ');
-    let numero2 = prompt('segundo numero para sumar: ');
-    resultado = sumar(numero1,numero2)
-    alert(`tu resultado es: ${resultado}`)
-} 
-else if (operacion == 2) {
-    let numero1 = prompt(' RESTA : ingrese el primer numero: ');
-    let numero2 = prompt('ingrese el segundo numero p/ restar: ');
-    resultado = restar(numero1, numero2)
-    alert(`tu resultado es: ${resultado}`)
-
-}
-else if (operacion == 3) {
-    let numero1 = prompt('ingrese el primer numero: ');
-    let numero2 = prompt('ingrese el 2do numero pa dividir: ');
-    resultado = dividir(numero1, numero2);
-    alert(`tu resultado es: ${resultado}`)
-
-}else if (operacion == 4) {
-    let numero1 = prompt('ingrese el numero 1: ');
-    let numero2 = prompt('ingrese el nro 2 para multiplicar: ');
-    resultado = multip(numero1, numero2);
-    alert(`tu resultado es: ${resultado}`)
-}
+class CelularAltaGama extends Celular {
+    constructor(color, peso, tamano, rdc, rcde, ram){
+        super(color, peso, tamano, rdc, rcde, ram);
+        this.resolucionDeCamarExtra = rcde;
+    }
+    grabarVideoLEnto(){
+        alert('estas grabando en camara lenta');
+    }
+    reconocimientoFacial(){
+        alert('se inicia el reconocimiento facial');
+    }
+    infoAltaGama(){
+       return this.mobileInfo + `Resolución de camara Extra: ${this.resolucionDeCamarExtra}`
+        }
+}   
 
 
-else { 
-    alert('esa operacion no esta mi reyyyyyy');
-}
+// celular1 = new Celular('rojo', '150 grm', '6 pulgadas', 'HD.', '2gb.');
+// celular2 = new Celular('rojo', '155 grm', '6.5 pulgadas', 'FULL HD.', '6gb.');
+// celular3 = new Celular('rojo', '160 grm', '7 pulgadas', 'HD ULTRA K.', '8gb.');
+
+// document.write(`
+// ${celular1.infoAltaGama()} <br>
+// ${celular2.infoAltaGama()} <br>
+// ${celular3.mobileInfo()} <br> `)
+
+// celular1.presionarBotonEncedido();
+// celular1.tomarFoto();
+// celular1.grabarVideo();
+// celular1.reiniciar();
+// celular1.presionarBotonEncedido();
+
+celular1 = new CelularAltaGama('rojo', '130gr', '5.3"', '4K', '3gb', 'full hd');
+celular2 = new CelularAltaGama('negro', '120gr', '5.0"', '4k', '4GB', 'hd ultra k');
+document.write(`
+${celular1.infoAltaGama()} <br>
+${celular2.infoAltaGama()} <br>`)
+ 
